dc336662d4aa3bd1264ab08c60481c979f19f6ab
b00mman <vincent.jude.clement@gmail.com>
2021-12-15 16:24:59 -0800

    works
    basically

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 from flask import Flask, render_template, url_for
 from flask_socketio import SocketIO, emit
+import json
 
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode=None)
+canvas = [0, 0]
 
 @app.route("/")
 def main():
     return render_template("index.html", sync_mode=socketio.async_mode)
 
-@socketio.event()
+@socketio.event
+def connect():
+    print("hi")
+    global canvas
+    print(canvas)
+    emit("draw", canvas)
+
+
+@socketio.event
 def draw(data):
+    global canvas
+    canvas = data
     emit("draw", data, broadcast=True, include_self=False)
 
 
